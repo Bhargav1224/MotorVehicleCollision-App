3d9c313f8c9284579eb3419a6d61aa180161ae64
Bhargav1224 <bhargavkudala@gmail.com>
2021-09-19 18:10:53 +0530

    developed UI for cards and details page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,40 +5,32 @@ import { useState } from "react";
 import { Redirect } from "react-router";
 //local imports
 import "./Home.css";
-import { LoaderSpinner } from "./Loader";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+
 import { loadData } from "../../Utilis/localstorage";
 
 export const Home = () => {
 	//Stored the data from API in carData
 	const [carData, setCarData] = useState([]);
 	const [date, setDate] = useState("");
-	//Loading,Error states used for Loading webpage when user onload the webpage
-	const [isLoading, setIsLoading] = useState(false);
-	const [isError, setIsError] = useState(false);
+
 	//Maintaining Pagination state
 	const [page, setPage] = useState(0);
-
-	const { username } = useSelector((state) => state.auth);
 
 	const isRegister = loadData("register");
 
 	//getting data from API
 	const getData = () => {
-		setIsLoading(true);
 		return axios
 			.get(
-				`https://data.cityofnewyork.us/resource/h9gi-nx95.json?$offset=${page}&$limit=6`
+				`https://data.cityofnewyork.us/resource/h9gi-nx95.json?$offset=${page}&$limit=8`
 			)
 			.then((res) => {
 				console.log(res.data);
-				setIsLoading(false);
-				setIsError(false);
 				setCarData(res.data);
 			})
 			.catch((er) => {
-				setIsError(true);
+				alert(er);
 			});
 	};
 
@@ -47,14 +39,18 @@ export const Home = () => {
 	const filterByDate = (e) => {
 		// e.preventDefault();
 
-		return axios
-			.get(
-				`https://data.cityofnewyork.us/resource/h9gi-nx95.json?crash_date=${date}&$offset=${page}&$limit=6`
-			)
-			.then((res) => {
-				setCarData(res.data);
-				// getData();
-			});
+		if (date.length > 1) {
+			return axios
+				.get(
+					`https://data.cityofnewyork.us/resource/h9gi-nx95.json?crash_date=${date}&$offset=${page}&$limit=6`
+				)
+				.then((res) => {
+					setCarData(res.data);
+					// getData();
+				});
+		} else {
+			getData();
+		}
 	};
 
 	//Debouncing method , when user hit the input field , It will reduce the function calls
@@ -83,13 +79,7 @@ export const Home = () => {
 		// eslint-disable-next-line
 	}, [page]);
 
-	return isLoading ? (
-		<LoaderSpinner />
-	) : isError ? (
-		<h2 style={{ color: "rgb(192, 45, 40)", textAlign: "center" }}>
-			404 Something went wrong
-		</h2>
-	) :isRegister? (
+	return isRegister ? (
 		<React.Fragment>
 			<div className="filter-container">
 				<input
@@ -99,7 +89,7 @@ export const Home = () => {
 					name="date"
 					onKeyUp={betterFn}
 					className="input-filter"
-					placeholder="filter based on date"
+					placeholder="Filter based on Date"
 				/>
 			</div>
 			<div className="container">
@@ -109,11 +99,21 @@ export const Home = () => {
 						key={item.collision_id}
 						className="mini-container"
 					>
-						<div>{item.vehicle_type_code1}</div>
-						<div>{item.vehicle_type_code2 && item.vehicle_type_code2}</div>
-						<div>{item.vehicle_type_code3 && item.vehicle_type_code3}</div>
-						<div>Time: {item.crash_time}</div>
-						<div>Date:{item.crash_date}</div>
+						<div className="mini-box">
+							<div className="card-content1">{item.vehicle_type_code1}</div>
+							<div className="card-content2">
+								{item.vehicle_type_code2
+									? `Car-2:${item.vehicle_type_code2}`
+									: ""}
+							</div>
+							<div className="card-content3">
+								{item.vehicle_type_code3
+									? `Car-3:${item.vehicle_type_code3}`
+									: ""}
+							</div>
+							<div className="card-content4">{item.crash_time}</div>
+							<div className="card-content5">{item.crash_date}</div>
+						</div>
 					</Link>
 				))}
 			</div>
@@ -130,5 +130,7 @@ export const Home = () => {
 				</button>
 			</div>
 		</React.Fragment>
-	):(<Redirect to="/"/>)
+	) : (
+		<Redirect to="/" />
+	);
 };
--- src/Components/Info/Information.jsx
+++ src/Components/Info/Information.jsx
@@ -28,10 +28,9 @@ export const Information = () => {
 			});
 	};
 
-	console.log(detailsData);
-
 	useEffect(() => {
 		collisionDetails();
+		// eslint-disable-next-line
 	}, []);
 
 	return isLoading ? (
@@ -44,21 +43,67 @@ export const Information = () => {
 		<div>
 			{detailsData?.map((item) => (
 				<div className="details-container">
-					<h1>Vehicle1-{item.contributing_factor_vehicle_1}</h1>
-					<p>Crash-Date: {item.crash_date}</p>
-					<p>Crash-Time :{item.crash_time}</p>
-					<p>Cyclist-Injured:{item.number_of_cyclist_injured}</p>
-					<p>Cyclist-Killed: {item.number_of_cyclist_killed}</p>
-					<p>Motorist-Injured: {item.number_of_motorist_injured}</p>
-                    <p>Motorist-Killed{ item.number_of_motorist_killed}</p>
-                    <p>Pedestrians-Injured:{item.number_of_pedestrians_injured}</p>
-					<p>Pedestrians-Killed:{item.number_of_pedestrians_killed}</p>
-					<p>Persons-Injured:{item.number_of_persons_injured}</p>
-					<p>Persons-Killed:{item.number_of_persons_killed}</p>
-					<p>Street-Name{item.on_street_name}</p>
-					<p>{item.vehicle_type_code1 && item.vehicle_type_code1}</p>
-					<p>{item.vehicle_type_code2 && item.vehicle_type_code2}</p>
-					<p>{item.vehicle_type_code3 && item.vehicle_type_code3}</p>
+					<p className="p1">
+						{item.borough ? `${item.borough}` : "No Information"}
+					</p>
+					<p className="p2">
+						{item.contributing_factor_vehicle_1
+							? `${item.contributing_factor_vehicle_1}`
+							: "No Information"}
+					</p>
+					<p className="p3">
+						{item.contributing_factor_vehicle_2
+							? `Contributed-Vehicle-2:${item.contributing_factor_vehicle_2}`
+							: "No Information"}
+					</p>
+					<p className="p4">
+						{item.contributing_factor_vehicle_3
+							? `Contributed-Vehicle-3:${item.contributing_factor_vehicle_3}`
+							: "No Information"}
+					</p>
+					<p className="p5">{item.crash_date}</p>
+					<p className="p6">{item.crash_time}</p>
+					<p className="p7">
+						{item.latitude ? `${item.latitude}` : "No Information"}
+					</p>
+					<p className="p8">
+						{item.longitude ? `${item.longitude}` : "No Information"}
+					</p>
+					<p className="p9">
+						{item.vehicle_type_code1
+							? `${item.vehicle_type_code1}`
+							: "No Information"}
+					</p>
+					<p className="p10">
+						{item.vehicle_type_code2
+							? `${item.vehicle_type_code2}`
+							: "No Information"}
+					</p>
+					<p className="p11">
+						{item.vehicle_type_code3
+							? `${item.vehicle_type_code3}`
+							: "No Information"}
+					</p>
+					<p className="p12">
+						{item.zip_code ? ` ${item.zip_code}` : "No Information"}
+					</p>
+					<p className="p13">{item.cross_street_name}</p>
+					<p className="p14">{item.number_of_cyclist_injured}</p>
+					<p className="p15">Cyclist-Killed: {item.number_of_cyclist_killed}</p>
+					<p className="p16">{item.number_of_motorist_injured}</p>
+					<p className="p17">{item.number_of_motorist_killed}</p>
+					<p className="p18">{item.number_of_pedestrians_injured}</p>
+					<p className="p19">{item.number_of_pedestrians_killed}</p>
+					<p className="p20">{item.number_of_persons_injured}</p>
+					<p className="p21"> {item.number_of_persons_killed}</p>
+					<p className="p22">
+						{item.off_street_name
+							? `${item.off_street_name}`
+							: "No Information"}
+					</p>
+					<p className="p23">
+						{item.on_street_name ? `${item.on_street_name}` : "No Information"}
+					</p>
 				</div>
 			))}
 		</div>
